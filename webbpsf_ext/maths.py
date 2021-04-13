6708b14f167c128680853cb0832c55ac391a8cc0
Jarron Leisenring <JarronL@users.noreply.github.com>
2021-04-13 16:17:10 -0700

    replace np.dot with np.matmul for increased speed

--- webbpsf_ext/maths.py
+++ webbpsf_ext/maths.py
@@ -97,7 +97,8 @@ def jl_poly(xvals, coeff, dim_reorder=False, use_legendre=False, lxmap=None, **k
         # Coefficients are assumed (deg+1,nx,ny)
         # xvals have length nz
         # Result to be ordered (nx,ny,nz)
-        yfit = np.dot(cf.T, xfan)
+        # Use np.matmul instead of np.dot for speed improvement
+        yfit = np.matmul(cf.T, xfan) # cf.T @ xfan
 
         if ndim==1 or n==1: 
             yfit = yfit.ravel()
@@ -108,7 +109,8 @@ def jl_poly(xvals, coeff, dim_reorder=False, use_legendre=False, lxmap=None, **k
         # Coefficients are assumed (deg+1,ny,nx)
         # xvals have length nz
         # Result to be ordered (nz,ny,nx)
-        yfit = np.dot(xfan.T,cf)
+        # Use np.matmul instead of np.dot for speed improvement
+        yfit = np.matmul(xfan.T, cf) # xfan.T @ cf
 
         if ndim==1 or n==1: 
             yfit = yfit.ravel()
@@ -207,14 +209,15 @@ def jl_poly_fit(x, yvals, deg=1, QR=True, robust_fit=False, niter=25, use_legend
     b = yvals.reshape([orig_shape[0],-1])
 
     # Fast method, but numerically unstable for overdetermined systems
-    #cov = np.linalg.pinv(np.dot(a,a.T))
-    #coeff_all = np.dot(cov,np.dot(a,b))
+    #cov = np.linalg.pinv(np.matmul(a,a.T))
+    #coeff_all = np.matmul(cov,np.matmul(a,b))
     
     if QR:
         # Perform QR decomposition of the A matrix
         q, r = np.linalg.qr(a.T, 'reduced')
         # computing Q^T*b (project b onto the range of A)
-        qTb = np.dot(q.T, b)
+        # Use np.matmul instead of np.dot for speed improvement
+        qTb = np.matmul(q.T,b) # q.T @ b 
         # solving R*x = Q^T*b
         coeff_all = np.linalg.lstsq(r, qTb, rcond=None)[0]
     else:
@@ -258,7 +261,8 @@ def jl_poly_fit(x, yvals, deg=1, QR=True, robust_fit=False, niter=25, use_legend
             ind_fit = outliers.sum(axis=0) > 0
             if ind_fit[ind_fit].size == 0: break
             if QR:
-                qTb = np.dot(q.T, yvals_fix[:,ind_fit])
+                # Use np.matmul instead of np.dot for speed improvement
+                qTb = np.matmul(q.T,yvals_fix[:,ind_fit]) # q.T @ yvals_fix[:,ind_fit] 
                 coeff_all[:,ind_fit] = np.linalg.lstsq(r, qTb, rcond=None)[0]
             else:
                 coeff_all[:,ind_fit] = np.linalg.lstsq(a.T, yvals_fix[:,ind_fit], rcond=None)[0]
