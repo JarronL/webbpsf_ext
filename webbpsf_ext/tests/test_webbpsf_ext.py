849341924a3d4f1347d3d29e1cda5f0bdc14c906
Jarron Leisenring <JarronL@users.noreply.github.com>
2024-02-01 18:16:36 -0700

    Update test_webbpsf_ext.py

--- webbpsf_ext/tests/test_webbpsf_ext.py
+++ webbpsf_ext/tests/test_webbpsf_ext.py
@@ -9,11 +9,12 @@ setup_logging(level='ERROR', verbose=False)
 
 sp_vega = synphot_ext.Vega
 
-def normalize_psf(arr, trim=3):
+def normalize_psf(arr, trim=3, max=False):
+    norm_func = np.max if max else np.sum
     if (trim is None) or (trim == 0):
-        return arr / arr.sum()
+        return arr / norm_func(arr)
     else:
-        return arr[trim:-trim,trim:-trim] / arr[trim:-trim,trim:-trim].sum()
+        return arr[trim:-trim,trim:-trim] / norm_func(arr[trim:-trim,trim:-trim])
 
 def test_monochromatic(nrc_f335m_wext, nrc_f335m_webbpsf):
     """Test that webbpsf_ext and webbpsf give the same results for monochromatic PSFs"""
@@ -49,15 +50,18 @@ def test_field_dependent_psfs(xsci, ysci, nrc_f335m_coeffs_cached, nrc_f335m_web
 
     # WebbPSF PSF at 'sci' coordinates
     nrc0 = nrc_f335m_webbpsf
+    nrc1 = nrc_f335m_coeffs_cached
+
     nrc0.detector_position = (xsci, ysci)
-    psf0 = nrc0.calc_psf(source=sp_vega, fov_pixels=33, oversample=2)
+    fov_pix = nrc1.fov_pix
+    osamp = nrc1.oversample
+    psf0 = nrc0.calc_psf(source=sp_vega, fov_pixels=fov_pix, oversample=osamp)
 
     # WebbPSF Extended PSF at 'sci' coordinates, native and using coefficients
-    nrc = nrc_f335m_coeffs_cached
-    psf1 = nrc.calc_psf(sp=sp_vega, return_oversample=False,
-                        coord_vals=(xsci, ysci), coord_frame='sci')
-    psf2 = nrc.calc_psf_from_coeff(sp=sp_vega, return_oversample=False,
-                                  coord_vals=(xsci, ysci), coord_frame='sci')
+    psf1 = nrc1.calc_psf(sp=sp_vega, return_oversample=False,
+                         coord_vals=(xsci, ysci), coord_frame='sci')
+    psf2 = nrc1.calc_psf_from_coeff(sp=sp_vega, return_oversample=False,
+                                   coord_vals=(xsci, ysci), coord_frame='sci')
     
     # Compare PSFs
     arr0 = normalize_psf(psf0[3].data)
@@ -101,13 +105,17 @@ def test_psfs_cached(nrc_f335m_webbpsf, nrc_f335m_wext, filter):
     assert np.allclose(arr1, arr2, atol=0.0001)
     assert np.allclose(arr1, arr3, atol=0.0001)
 
-
-def test_coron_psfs(nrc_f335m_coeffs_cached):
+@pytest.mark.parametrize("xidl, yidl", [(0,0), (0.1,0.1), (1,1)])
+def test_coron_psfs(xidl, yidl, nrc_f335m_coeffs_cached):
 
     nrc = nrc_f335m_coeffs_cached
 
-    psf1 = nrc.calc_psf(sp=sp_vega)
-    psf2 = nrc.calc_psf_from_coeff(sp=sp_vega, return_oversample=False)
+    # psf1 = nrc.calc_psf(sp=sp_vega)
+    # psf2 = nrc.calc_psf_from_coeff(sp=sp_vega, return_oversample=False)
+
+    psf1 = nrc.calc_psf(sp=sp_vega, coord_vals=(xidl,yidl), coord_frame='idl')
+    psf2 = nrc.calc_psf_from_coeff(sp=sp_vega, return_oversample=False,
+                                   coord_vals=(xidl,yidl), coord_frame='idl')
 
     # Compare PSFs
     arr1 = normalize_psf(psf1[3].data)
